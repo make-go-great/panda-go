e4abba5e24a7cab15451dc3474c706dd87d884a8
hau <ngtranhau@gmail.com>
2020-12-25 14:31:57 +0700

    feat: add stack

---
+++ pkg/stack/stack_int.go
@@ -0,0 +1,34 @@
+package stack
+
+type StackInt struct {
+	arr []int
+}
+
+func NewStackInt() *StackInt {
+	return &StackInt{
+		arr: make([]int, 0, 10),
+	}
+}
+
+func (s *StackInt) Push(v int) {
+	s.arr = append(s.arr, v)
+}
+
+func (s *StackInt) Peek() (int, bool) {
+	if len(s.arr) == 0 {
+		return 0, false
+	}
+
+	result := s.arr[len(s.arr)-1]
+	return result, true
+}
+
+func (s *StackInt) Pop() (int, bool) {
+	if len(s.arr) == 0 {
+		return 0, false
+	}
+
+	result := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return result, true
+}
---
+++ pkg/stack/stack_string.go
@@ -0,0 +1,38 @@
+package stack
+
+const (
+	defaultStackStringLen = 100
+)
+
+type StackString struct {
+	arr []string
+}
+
+func NewStackString() *StackString {
+	return &StackString{
+		arr: make([]string, 0, defaultStackStringLen),
+	}
+}
+
+func (s *StackString) Push(v string) {
+	s.arr = append(s.arr, v)
+}
+
+func (s *StackString) Peek() (string, bool) {
+	if len(s.arr) == 0 {
+		return "", false
+	}
+
+	result := s.arr[len(s.arr)-1]
+	return result, true
+}
+
+func (s *StackString) Pop() (string, bool) {
+	if len(s.arr) == 0 {
+		return "", false
+	}
+
+	result := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return result, true
+}
