53f395906b5672ba6ce25c56ee2a93d2be66754f
Hau Nguyen <hauvipapro@gmail.com>
2021-12-02 14:55:01 +0700

    feat: add ParseIntLines

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,9 @@ package parser
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -26,4 +28,29 @@ func ParseParagraphs(filename string) ([]string, error) {
 	lines := strings.Split(strings.TrimSpace(string(bytes)), "\n\n")
 
 	return lines, nil
+}
+
+func ParseIntLines(filename string) ([]int, error) {
+	lines, err := ParseLines(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse lines: %w", err)
+	}
+
+	result := make([]int, 0, len(lines))
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Println("failed to atoi", line, err)
+			continue
+		}
+
+		result = append(result, num)
+	}
+
+	return result, nil
 }
